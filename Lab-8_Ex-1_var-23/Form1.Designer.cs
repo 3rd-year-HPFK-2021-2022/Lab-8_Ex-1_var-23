a7bc653bec09f58f0361f58e4c3fd2ce69b833ae
Vlad <78665125+Bambalela@users.noreply.github.com>
2021-11-28 15:06:01 +0200

    finish, i think

--- Lab-8_Ex-1_var-23/Form1.Designer.cs
+++ Lab-8_Ex-1_var-23/Form1.Designer.cs
@@ -34,6 +34,12 @@
             this.Value1_text = new System.Windows.Forms.TextBox();
             this.Value2_text = new System.Windows.Forms.TextBox();
             this.ReplaceButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // ValueCC1
@@ -52,7 +58,7 @@
             this.ValueCC2.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
             this.ValueCC2.FormattingEnabled = true;
             this.ValueCC2.IntegralHeight = false;
-            this.ValueCC2.Location = new System.Drawing.Point(285, 49);
+            this.ValueCC2.Location = new System.Drawing.Point(369, 49);
             this.ValueCC2.Name = "ValueCC2";
             this.ValueCC2.Size = new System.Drawing.Size(121, 23);
             this.ValueCC2.TabIndex = 2;
@@ -65,12 +71,13 @@
             this.Value1_text.Name = "Value1_text";
             this.Value1_text.Size = new System.Drawing.Size(121, 23);
             this.Value1_text.TabIndex = 1;
+            this.Value1_text.TextChanged += new System.EventHandler(this.Value1_text_TextChanged);
             this.Value1_text.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Value1_text_KeyPress);
             // 
             // Value2_text
             // 
             this.Value2_text.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
-            this.Value2_text.Location = new System.Drawing.Point(285, 161);
+            this.Value2_text.Location = new System.Drawing.Point(369, 161);
             this.Value2_text.Name = "Value2_text";
             this.Value2_text.ReadOnly = true;
             this.Value2_text.Size = new System.Drawing.Size(121, 23);
@@ -80,30 +87,101 @@
             // ReplaceButton
             // 
             this.ReplaceButton.Image = global::Lab_8_Ex_1_var_23.Properties.Resources.circle;
-            this.ReplaceButton.Location = new System.Drawing.Point(187, 100);
+            this.ReplaceButton.Location = new System.Drawing.Point(223, 85);
             this.ReplaceButton.Name = "ReplaceButton";
             this.ReplaceButton.Size = new System.Drawing.Size(55, 55);
             this.ReplaceButton.TabIndex = 5;
             this.ReplaceButton.UseVisualStyleBackColor = true;
+            this.ReplaceButton.Click += new System.EventHandler(this.ReplaceButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.label1.Location = new System.Drawing.Point(23, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 21);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Валюта:";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.label2.Location = new System.Drawing.Point(369, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 21);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Валюта:";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.label3.Location = new System.Drawing.Point(23, 137);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 21);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Кількість:";
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.label4.Location = new System.Drawing.Point(369, 137);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 21);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Кількість:";
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.label5.Location = new System.Drawing.Point(23, 75);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(185, 21);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.label6.Location = new System.Drawing.Point(369, 75);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(185, 21);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "label6";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(443, 233);
+            this.ClientSize = new System.Drawing.Size(515, 237);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.ReplaceButton);
             this.Controls.Add(this.Value2_text);
             this.Controls.Add(this.Value1_text);
             this.Controls.Add(this.ValueCC2);
             this.Controls.Add(this.ValueCC1);
             this.Location = new System.Drawing.Point(15, 15);
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.ShowIcon = false;
             this.Text = "Lab-8_ValueConverter";
             this.ResumeLayout(false);
             this.PerformLayout();
         }
+
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+
+        private System.Windows.Forms.Label label1;
 
         private System.Windows.Forms.Button ReplaceButton;
 
@@ -114,14 +192,7 @@
         private System.Windows.Forms.ComboBox ValueCC2;
 
         private System.Windows.Forms.ComboBox ValueCC1;
-
-        private System.Windows.Forms.ComboBox comboBox2;
-
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.ComboBox comboBox1;
-
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        
 
         #endregion
     }
--- Lab-8_Ex-1_var-23/Form1.cs
+++ Lab-8_Ex-1_var-23/Form1.cs
@@ -11,28 +11,36 @@ namespace Lab_8_Ex_1_var_23
 {
     public class Values
     {
-        private string _name;
-        private double _rate;
-
         public Values(string name, double rate)
         {
-            _name = name;
-            _rate = rate;
+            Name = name;
+            Rate = rate;
         }
-    }
+        public string Name { get; }
 
+        public double Rate { get; }
+    }
+    
     public partial class Form1 : Form
     {
-        private Dictionary<string, Values> Exchange = new Dictionary<string, Values>();
-        private bool isReadyToCalculate = false;
+        private readonly Dictionary<string, Values> _exchange = new Dictionary<string, Values>();
+
+        private bool _buttonReplacePush = false;
+        //private double ValueBox1 = 0, ValueBox2 = 0;
+        void AddToCombobox(string cc, string txt)
+        {
+            var comboboxItem = cc/* + ' ' + txt*/;
+            ValueCC1.Items.Add(comboboxItem!);
+            ValueCC2.Items.Add(comboboxItem!);
+        }
         public Form1()
         {
             InitializeComponent();
-            const string UrlString = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchangenew";
+            const string urlString = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchangenew";
             const string file = "exchanger.xml";
             
 
-            var reader = new XmlTextReader(UrlString);
+            var reader = new XmlTextReader(urlString);
             if (reader.HasValue)
             {
                 StreamWriter sw = new StreamWriter(file);
@@ -59,8 +67,7 @@ namespace Lab_8_Ex_1_var_23
 
                 sw.Close();
             }
-
-
+            
             var xmlDocument = new XmlDocument();
             xmlDocument.Load(file);
             var xRoot = xmlDocument.DocumentElement;
@@ -78,18 +85,19 @@ namespace Lab_8_Ex_1_var_23
                             rate = double.Parse(childNode.InnerText, NumberStyles.Any, CultureInfo.InvariantCulture);
                     }
                     //MessageBox.Show($"cc: {cc} \ttxt: {txt} \trate: {rate}");
-                    if (txt != null && cc != null) Exchange?.Add(cc, new Values(txt, rate));
-                    var comboboxItem = cc/* + ' ' + txt*/;
-                    ValueCC1.Items.Add(comboboxItem!);
-                    ValueCC2.Items.Add(comboboxItem!);
+                    _exchange?.Add(cc, new Values(txt, rate));
+                    AddToCombobox(cc,txt);
                 }
             }
 
+            _exchange?.Add("UAH",new Values("Українська гривня" , 1));
+            AddToCombobox("UAH","Українська гривня");
             ValueCC1.Size = ValueCC2.Size = Value1_text.Size = Value2_text.Size = new Size(130, 40);
-            Value1_text.Enabled = Value2_text.Enabled = ReplaceButton.Enabled = false;
+            Value1_text.Enabled = ReplaceButton.Enabled = false;
+            label5.Text = label6.Text = "";
         }
         
-        private void KeyPress(object sender, KeyPressEventArgs e, Control textBox)
+        private new void KeyPress(object sender, KeyPressEventArgs e, Control textBox)
         {
             if (char.IsDigit(e.KeyChar) || char.IsControl(e.KeyChar) || e.KeyChar is ',' or '.' || e.KeyChar == 8)
             {
@@ -111,22 +119,60 @@ namespace Lab_8_Ex_1_var_23
             KeyPress(sender, e, Value2_text);
         }
 
-        private void SelectedIndexChanged(ComboBox comboBox,TextBox textBox)
+        private void SelectedIndexChanged(ComboBox comboBox, Label label, object sender, EventArgs e)
         {
-            if (comboBox.SelectedIndex != -1) textBox.Enabled = true;
-            if (Value2_text.Enabled && Value2_text.Enabled)
+            if (ValueCC1.SelectedIndex != -1 && ValueCC2.SelectedIndex != -1) {
+                Value1_text.Enabled = true;
+                ReplaceButton.Enabled = true;
+            }
+            label.Text = _exchange[comboBox.SelectedItem.ToString()].Name;
+            
+            if (ValueCC1.SelectedIndex != CC1_previous && ValueCC1.SelectedIndex != CC2_previous)
             {
-                ReplaceButton.Enabled = isReadyToCalculate = true;
+                Value1_text_TextChanged(sender,e);
             }
         }
+
+        private int CC1_previous = -1, CC2_previous = -1;
         private void ValueCC1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            SelectedIndexChanged(ValueCC1, Value1_text);
+            SelectedIndexChanged(ValueCC1, label5, sender, e);
+            
         }
 
         private void ValueCC2_SelectedIndexChanged(object sender, EventArgs e)
         {
-            SelectedIndexChanged(ValueCC2, Value2_text);
+            SelectedIndexChanged(ValueCC2, label6, sender, e);
+           
+        }
+
+        private void ReplaceButton_Click(object sender, EventArgs e)
+        {
+            CC1_previous = ValueCC1.SelectedIndex;
+            CC2_previous = ValueCC2.SelectedIndex;
+            (ValueCC1.SelectedIndex, ValueCC2.SelectedIndex) = (ValueCC2.SelectedIndex, ValueCC1.SelectedIndex);
+            Value1_text.Text = Value2_text.Text;
+            
+            Value1_text_TextChanged(sender, e);
+        }
+
+        private void Value1_text_TextChanged(object sender, EventArgs e)
+        {
+            if (Value1_text.Text.Length != decimal.Zero)
+            {
+                try
+                {
+                    var value1Double = double.Parse(Value1_text.Text);
+                    double toUAH = value1Double * _exchange[ValueCC1.SelectedItem.ToString()].Rate;
+                    double toOtherValue = toUAH / _exchange[ValueCC2.SelectedItem.ToString()].Rate;
+                    Value2_text.Text = String.Format("{0:f}",toOtherValue);
+                    //MessageBox.Show(toOtherValue.ToString());
+                }
+                catch (Exception exc)
+                {
+                    MessageBox.Show(exc.Message);
+                }
+            }
         }
     }
 }
